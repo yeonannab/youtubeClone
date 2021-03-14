c6f1bb67f564a2d1b8c44c956c065c1111695bad
anna baek <yeon376@yonsei.ac.kr>
2021-03-15 04:01:08 +0900

    10주차

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,67 @@
+import React,{ useEffect, useState } from 'react'
+import { FaCode } from "react-icons/fa";
+import { Card, Icon, Avatar, Col, Typography, Row } from 'antd';
+import moment from 'moment';
+import Axios from 'axios';
+const {Title} = Typography
+const {Meta} = Card;
+
+function LandingPage() {
+
+    const [Video, setVideo] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+        .then(response => {
+            if(response.data.success) {
+                console.log(response.data)
+                setVideo(response.data.videos)
+            } else {
+                alert('비디오 가져오기를 실패했습니다.')
+            }
+        })
+    }, [])
+    
+    const renderCards = Video.map((video, index)=>{
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - minutes * 60));
+
+        return <Col key={index} lg={6} md={8} xs={24}>
+            <a href={`/video/post/${video._id}`}>
+                <div style={{position: 'relative'}}>
+                    <img style={{width: '100%'}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                    <div className="duration">
+                        <span>{minutes} : {seconds}</span>
+                    </div>
+                </div>
+            </a>
+            <br />
+            <Meta
+                avatar={
+                    <Avatar src={video.writer.image} />
+                }
+                title={video.title}
+                description=""
+            />
+            <span>{video.writer.name}</span><br/>
+            <span style={{ marginLeft: '3rem' }}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+        </Col>
+
+
+
+    })
+
+    return (
+        <div style={{ width: '85%', margin: '3rem auto'}}>
+            <Title level={2}> Recommended </Title>
+            <hr />
+            <Row gutter={[32, 16]}>
+                {renderCards}
+            </Row>
+        </div>
+
+    )
+}
+
+export default LandingPage
---
+++ server/routes/video.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const { Video } = require("../models/Video");
+
+const multer = require("multer");
+const { auth } = require("../middleware/auth");
+var ffmpeg = require("fluent-ffmpeg");
+
+// storage multer config
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.mp4') {
+            return cb(res.status(400).end('only mp4 is allowed'), false);
+        }
+        cb(null, true)
+    }
+});
+
+const upload = multer({storage: storage}).single("file");
+
+
+
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+
+    // 비디오를 서버에 저장한다.
+    upload(req, res, err => {
+        if(err) {
+            return res.json({ success: false, err})
+        }
+        return res.json({ success: true, url: res.req.file.path, fileName: res.req.file.filename})
+    })
+
+})
+
+router.post('/uploadVideo', (req, res) => {
+
+    // 비디오를 정보들을 저장한다.
+    const video = new Video(req.body)
+
+    video.save((err, doc)=> {
+        if(err) return res.json({success: false, err})
+        res.status(200).json({success: true})
+    })
+    
+})
+
+router.get('/getVideos', (req, res) => {
+
+    // 비디오를 DB에서 가져와서 클라이언트에 보낸다.
+    Video.find()
+        .populate('writer')
+        .exec((err, videos)=>{
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+    
+})
+
+router.post('/thumbnail', (req, res) => {
+
+    // 썸네일 생성하고 비디오 러닝타임도 가져오기
+    let filePath = ""
+    let fileDuration = ""
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+        console.dir(metadata); // all metadata
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration
+    });
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+        console.log('Will generate' + filenames.join(', '))
+        console.log(filenames)
+
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end', function () {
+        console.log('Screenshots taken');
+        return res.json({ success: true, url: filePath, fileDuration: fileDuration });
+    })
+    .on('error', function (err) {
+        console.error(err);
+        return res.json({ success: false, err});
+    })
+    .screenshots({
+        // Will take screenshots at 20%, 40%, 60% and 80% of the video
+        count: 3,
+        folder: 'uploads/thumbnails',
+        size: '320x240',
+        // '%b' : input basename (filename w/o extension)
+        filename: 'thumbnail-%b.png'
+    })
+
+})
+
+
+
+
+module.exports = router;
